b8c90e73bd7f60f758320885ca678bbb8f0cd966
zx <1043943494@qq.com>
2017-03-20 18:14:13 +0800

    增加302test代码

--- server/server.js
+++ server/server.js
@@ -1,4 +1,18 @@
 var express = require('express');
 var app = express();
 app.use(express.static('../'));
+app.use(express.static('./app'));
+
+
+//测试301
+app.get('/test301', function(req, res) {
+    res.redirect(301, 'https://www.baidu.com');
+});
+
+//测试302
+app.get('/test302', function(req, res) {
+    res.redirect(302, 'https://www.baidu.com');
+});
+
 app.listen(3000);
+console.log('服务启动!!!!','端口3000', 'localhost:3000');
