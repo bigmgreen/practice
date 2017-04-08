115d61b75dfa10fb3d83d54c04bca31044c0c823
dqs <bigmgreen@163.com>
2017-04-08 23:19:59 +0800

    10.1 fis3重组百度首页完成

--- lesson-10/1.baidu/fis-conf.js
+++ lesson-10/1.baidu/fis-conf.js
@@ -1,68 +1,3 @@
-//
-// /*    合并   start      */
-//
-// //js，css合并
-// fis.match('::package', {
-//     // 启用 fis-spriter-csssprites 插件
-//     spriter: fis.plugin('csssprites'),
-//     packager: fis.plugin('deps-pack', {
-//         'app.js': [
-//             '/static/lib/jquery/jquery.min.js',
-//             '/static/lib/storage/storage.js',
-//             '/static/lib/throttle/throttle.js',
-//             '/static/js/index.js'
-//         ],
-//         'app.css': [
-//             '/static/css/base.css',
-//             '/static/css/header.css',
-//             '/static/css/footer.css',
-//             '/static/css/skin.css',
-//             '/static/css/index.css'
-//         ]
-//     })
-// });
-//
-// // 对 CSS 进行图片合并
-// fis.match('/static/css/*.css', {
-//     // 给匹配到的文件分配属性 `useSprite`
-//     useSprite: true
-// });
-//
-// /*    合并   end      */
-//
-// /*   压缩   start   */
-//
-// // //对js进行压缩
-// // fis.match('/static/js/*.js', {
-// //     // fis-optimizer-uglify-js 插件进行压缩，已内置
-// //     optimizer: fis.plugin('uglify-js')
-// // });
-// //
-// // //对css进行压缩
-// // fis.match('/static/css/*.css', {
-// //     // fis-optimizer-clean-css 插件进行压缩，已内置
-// //     optimizer: fis.plugin('clean-css')
-// // });
-//
-// //对图片进行压缩
-// fis.match('*.{png,jpg}', {
-//     // fis-optimizer-png-compressor 插件进行压缩，已内置
-//     optimizer: fis.plugin('png-compressor')
-// });
-
-//对html进行压缩
-fis.match('*.html', {
-    //invoke fis-optimizer-html-minifier
-    optimizer: fis.plugin('html-minifier')
-});
-
-// /*   压缩   end   */
-
-// 加 md5
-fis.match('*.{js,css,png,jpg}', {
-    useHash: true
-});
-
 //发布的时候忽略以下目录或文件
 fis.set('project.ignore', [
     'package.json',
@@ -70,30 +5,39 @@ fis.set('project.ignore', [
     'fis-conf.js'
 ]);
 
-//
-// // 启用打包插件，必须匹配 ::package
-// fis.match('::package', {
-//     packager: fis.plugin('map'),
-//     spriter: fis.plugin('csssprites', {
-//         layout: 'matrix',
-//         margin: '15'
-//     })
-// })
-//     .match('*.js', {
-//         packTo: '/static/app.js',
-//         optimizer: fis.plugin('uglify-js')
-//     })
-//     .match('*.css', {
-//         packTo: '/static/app.css',
-//         optimizer: fis.plugin('clean-css')
-//     });
-
 fis.match('::package', {
     postpackager: fis.plugin('loader', {
         allInOne: true
+    }),
+    spriter: fis.plugin('csssprites'),
+    packager: fis.plugin('deps-pack', {
+        useTrack : false,
+        'output/app.js': [
+            '/static/lib/jquery/jquery.min.js',
+            '/static/lib/storage/storage.js',
+            '/static/lib/throttle/throttle.js',
+            '/static/js/index.js'
+        ],
+        'output/app.css': [
+            '/static/css/base.css',
+            '/static/css/header.css',
+            '/static/css/footer.css',
+            '/static/css/skin.css',
+            '/static/css/sprite.css',
+            '/static/css/index.css'
+        ]
     })
-}).match('/static/**.js', {
-    packTo: 'app.js'
-}).match('/static/**.css', {
-    packTo: 'app.css'
+}).match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+}).match('*.css', {
+    useSprite: true,
+    optimizer: fis.plugin('clean-css')
+}).match('*.{png,jpg}', {
+    optimizer: fis.plugin('png-compressor')
+}).match('output/**.{js,css，png,jpg}', {
+    useHash: true
+}).match('*.{png,jpg}', {
+    useHash: true
+}).match('*.html', {
+    optimizer: fis.plugin('html-minifier')
 });
