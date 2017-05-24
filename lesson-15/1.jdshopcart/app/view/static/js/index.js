dd44a6a7334c9d37f62a58a734abf665f16c7801
dqs <bigmgreen@163.com>
2017-05-24 23:34:59 +0800

    15.筛选完成

--- lesson-15/1.jdshopcart/app/view/static/js/index.js
+++ lesson-15/1.jdshopcart/app/view/static/js/index.js
@@ -45,15 +45,23 @@ var app = angular.module('shopCart', []);
 app.controller('shopCartController', function ($scope, $http) {
     $scope.recommendList = [];
     $scope.shopCart = [];
+    $scope.selectText = '全部商品';
+    $scope.selected = ['selected'];
     $http.get('/queryData').success(function (data) {
         $scope.shopCart = data.shopCart;
         $scope.recommendList = data.recommendList;
         $scope.isShow = false;
     });
 
-    $scope.queryShopCart = function (type) {
+    $scope.queryShopCart = function (type, text) {
         $scope.isShow = false;
-        $http.get('/queryShopCart', {params:{type: type}}).success(function (data) {
+        $scope.selectText = text;
+
+        var arr = new Array(type + 1);
+        arr[type] = 'selected';
+        $scope.selected = arr;
+
+        $http.get('/queryShopCart', {params: {type: type}}).success(function (data) {
             $scope.shopCart = data;
         });
     }
